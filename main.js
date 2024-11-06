bb7f58ab501de986b7bc292d900ff51e24549ea7
rizbinass <zerefkaigo12@gmail.com>
2024-11-06 18:03:02 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var jmlhAngkot = 10;
+var angkotOperasi = 6
+var angkot = 1;
+
+for(angkot; angkot <= jmlhAngkot; angkot++){
+    if(angkot <= angkotOperasi){
+        console.log('ada ' + angkot + ' yang operasi')
+    }else{
+        console.log('ada ' + angkot + ' yang nganggur')
+    }
+}
+
+            while(imNotSuccess){
+                keepTryingHard();
+            }
